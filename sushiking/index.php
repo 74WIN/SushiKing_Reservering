da7de086fb644bd66d8b688fb4622c07c8555a01
74WINXD <ccw.la@outlook.com>
2020-12-09 15:59:24 +0100

    Ik heb hover een kleur gegeven en de afstand tussen reserveren, verwijderen en aanpassen aangepast.

--- sushiking/index.php
+++ sushiking/index.php
@@ -15,13 +15,13 @@
             <img src="https://www.mountainspring-sushiking.nl/img/logo-transparent.243a420c.png" width = 350px; height=" 220px">
             <a/>
     </div>
-    <div class="topnav">
+    <nav>
         <a class="active" href="winkelmandje">Winkelmandje</a>
         <a href="#Bezorgen">Bezorgen</a>
         <a href="#Contact">Contact</a>
         <a href="#Menukaart">Menukaart</a>
         <a href="#Home">Home</a>
-    </div>
+    </nav>
 </header>
 <!--In de form hieronder kan jij de reservering plaatsen op de db-->
 <div class="main">
@@ -41,7 +41,7 @@
             <div>
                 <label for="telefoonnummer">telefoonnummer:</label>
 
-                <input type="telefoonnummer" id="telefoonnummer" name="telefoonnummer">
+                <input type="tel" id="telefoonnummer" name="telefoonnummer">
             </div>
             <div>
                 <label for="opmerking">opmerking:</label>
@@ -50,7 +50,7 @@
             </div>
             <div>
                 <label for="date">Datum:</label>
-                <input type="date" id="date" name="date">
+                <input type="date" id="txtDate" min="<?php echo date("Y-m-d"); ?>">
             </div>
             <label for="tijd">Tijd:</label>
             <select id="tijd" name="tijd">
@@ -96,41 +96,92 @@
                     <td><input type="text" id="opmerking" name="opmerking"/></td>
                 </tr>
             </table> -->
+            <br>
             <input type="submit" value="Reserveer"/>
         </form>
         <section/>
-        <br>
+
         <!--In de form hieronder kan jij de reservering verwijderen op de db-->
-        <h1>Verwijderen</h1>
-        <form method="post" action="">
-            E-mail: 	<input type="text" id="delEmail" name="delEmail"/><br>
-            <input type="submit" value="Delete"/>
-        </form>
+        <div id="verwijderen">
+            <h1>Verwijderen</h1>
+            <form method="post" action="">
+                E-mail: 	<input type="text" id="delEmail" name="delEmail"/><br>
+                <input type="submit" value="Delete"/>
+            </form>
+        </div>
         <br>
         <!--In de form hieronder kan jij de reservering aanpassen op de db-->
-        <h1>Aanpassen</h1>
-        <form method="post" action="">
-            <table>
-                <tr>
-                    <td>Naam </td>
-                    <td>Telefoon/Mobiel </td>
-                    <td>Tijdslot </td>
-                    <td>Aantal personen </td>
-                    <td>E-mail </td>
-                    <td>Opmerking </td>
-                </tr>
-                <tr>
-                    <td><input type="text" id="updNaam" name="updNaam"/></td>
-                    <td><input type="text" id="updTelefoonnummer" name="updTelefoonnummer"/></td>
-                    <td><input type="text" id="updTijd" name="updTijd"/></td>
-                    <td><input type="text" id="updPersonen" name="updPersonen"/></td>
-                    <td><input type="text" id="updEmail" name="updEmail"/></td>
-                    <td><input type="text" id="updOpmerking" name="updOpmerking"/></td>
-                </tr>
-            </table>
-            <input type="submit" value="Aanpassen"/>
+        <div id="aanpassen">
+            <h1>Aanpassen</h1>
+            <form method="post" action="">
+
+                <div>
+                    <label for="name">Name:</label>
+
+                    <input type="text" id="updNaam" name="name" required>
+                </div>
+                <div>
+                    <label for="email">email:</label>
+
+                    <input type="email" id="updEmail" name="updEmail" >
+                </div>
+                <div>
+                    <label for="telefoonnummer">telefoonnummer:</label>
+
+                    <input type="tel" id="updTelefoonnummer" name="telefoonnummer">
+                </div>
+                <div>
+                    <label for="opmerking">opmerking:</label>
+
+                    <input type="text" id="updOpmerking" name="updOpmerking">
+                </div>
+                <div>
+                    <label for="date">Datum:</label>
+                    <input type="date" id="txtDate" min="<?php echo date("Y-m-d"); ?>">
+                </div>
+                <label for="tijd">Tijd:</label>
+                <select id="updTijd" name="updTijd">
+                    <option value="17:00">17:00</option>
+                    <option value="20:00">20:00</option>
+                </select>
+                <label for="updPersonen">personen:</label>
+                <select id="updPersonen" name="updPersonen">
+                    <option value=1>1 persoon</option>
+                    <option value=2">2 personen</option>
+                    <option value=3">3 personen</option>
+                    <option value=4">4 personen</option>
+                    <option value=5">5 personen</option>
+                    <option value=6">6 personen</option>
+                    <option value=7">7 personen</option>
+                    <option value=8">8 personen</option>
+                    <option value=9">9 personen</option>
+                    <option value=10">10 personen</option>
+                </select>
+
+
+                <!--<table>
+                    <tr>
+                        <td>Naam </td>
+                        <td>Telefoon/Mobiel </td>
+                        <td>Tijdslot </td>
+                        <td>Aantal personen </td>
+                        <td>E-mail </td>
+                        <td>Opmerking </td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" id="updNaam" name="updNaam"/></td>
+                        <td><input type="text" id="updTelefoonnummer" name="updTelefoonnummer"/></td>
+                        <td><input type="text" id="updTijd" name="updTijd"/></td>
+                        <td><input type="text" id="updPersonen" name="updPersonen"/></td>
+                        <td><input type="text" id="updEmail" name="updEmail"/></td>
+                        <td><input type="text" id="updOpmerking" name="updOpmerking"/></td>
+                    </tr>
+                </table>-->
+                <input type="submit" value="Aanpassen"/>
+        </div>
 </div>
 </body>
 </html>
+
 
 
