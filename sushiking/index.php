e3573eed9117abffc79b03dc1129ee179b79bd7d
74WINXD <ccw.la@outlook.com>
2021-01-18 10:09:42 +0100

    Ik heb de tijden verandert

--- sushiking/index.php
+++ sushiking/index.php
@@ -34,9 +34,9 @@
                 <option value="12:00">12:00</option>
                 <option value="13:00">13:00</option>
                 <option value="14:00">14:00</option>
-                <option value="15:00">20:00</option>
-                <option value="16:00">12:00</option>
-                <option value="17:00">13:00</option>
+                <option value="15:00">15:00</option>
+                <option value="16:00">16:00</option>
+                <option value="17:00">17:00</option>
                 <option value="19:45">19:45</option>
             </select>
             <br>
